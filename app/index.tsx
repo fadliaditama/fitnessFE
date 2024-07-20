7852b88e4fbef32526f065ccca51ee0c0b5ae4fa
Acmad Fadli Aditama <37434467+fadliaditama@users.noreply.github.com>
2024-07-20 21:42:46 +0700

    Starterpack React Native + Tailwindcss

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-4xl text-center">walang.</Text>
+    </View>
+  );
+}
