22df1bb3bd6b76f23857648281e1e6236d4683c2
Acmad Fadli Aditama <37434467+fadliaditama@users.noreply.github.com>
2024-07-21 18:32:13 +0700

    fix env

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
+    <View className="flex-1 justify-center items-center">
       <Text className="text-4xl text-center">walang.</Text>
     </View>
   );
